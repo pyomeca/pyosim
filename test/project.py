f2d801190529778c90894d8dc9842f804bbfb250
Romain Martinez <martinez.staps@gmail.com>
2018-04-04 17:37:44 -0400

    project test files
    project test
    
    project test

--- test/project.py
+++ test/project.py
@@ -1,1 +1,20 @@
-# TODO: project test
+import shutil
+from pathlib import Path
+
+from pyosim.project import Project
+
+PROJECT_PATH = Path('./project_sample')
+# remove project if already exist
+if PROJECT_PATH.is_dir():
+    shutil.rmtree(PROJECT_PATH)
+
+# create Project object
+project = Project(path=PROJECT_PATH)
+
+# create project directory
+project.create_project()
+
+# add two participants from the template conf file
+TEMPLATE_PATH = Path('./data/') / '_conf.csv'
+shutil.copy(TEMPLATE_PATH, PROJECT_PATH)
+project.update_participants()
