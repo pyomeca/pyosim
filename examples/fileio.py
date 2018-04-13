d0df5d899dfc328919a7d2537beaf3f6d739a09f
Romain Martinez <martinez.staps@gmail.com>
2018-04-13 16:44:52 -0400

    implement to Analogs3dOsim to sto

--- examples/fileio.py
+++ examples/fileio.py
@@ -1,20 +1,28 @@
 from pathlib import Path
 
 from pyomeca import fileio as pyoio
-from pyosim.types import Markers3dOsim
+from pyosim.types import Markers3dOsim, Analogs3dOsim
 
 # Path to data
 DATA_FOLDER = Path('..') / 'tests' / 'data'
 MARKERS_ANALOGS_C3D = DATA_FOLDER / 'markers_analogs.c3d'
-TRC = DATA_FOLDER / 'markers.trc'
+TRC_OUT = DATA_FOLDER / 'markers.trc'
+STO_OUT = DATA_FOLDER / 'analogs.sto'
 
 # remove file if already exists
-if TRC.is_file():
-    TRC.unlink()
+if TRC_OUT.is_file():
+    TRC_OUT.unlink()
 
-# read c3d
+# read markers from c3d
 m = pyoio.read_c3d(MARKERS_ANALOGS_C3D, idx=[0, 1, 2, 3], kind='markers', prefix=':')
 # convert pyomeca's Markers3d to pyosim's Markers3dOsim
 mosim = Markers3dOsim(m)
 # write a trc file
-mosim.to_trc(file_name=TRC)
+mosim.to_trc(file_name=TRC_OUT)
+
+# read analogs from c3d
+a = pyoio.read_c3d(MARKERS_ANALOGS_C3D, names=['1', '2', '3', '4', '5', '6'], kind='analogs', prefix=':')
+# convert pyomeca's Analogs3d to pyosim's Analogs3dOsim
+aosim = Analogs3dOsim(a)
+# write a sto file
+aosim.to_sto(file_name=STO_OUT)
--- pyosim/types.py
+++ pyosim/types.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import numpy as np
 import opensim as osim
 
-from pyomeca.types import Markers3d
+from pyomeca.types import Markers3d, Analogs3d
 
 
 class Markers3dOsim(Markers3d):
@@ -63,4 +63,44 @@ class Markers3dOsim(Markers3d):
             table.appendRow(time_vector[iframe], row)
 
         adapter = osim.TRCFileAdapter()
+        adapter.write(table, str(file_name))
+
+
+class Analogs3dOsim(Analogs3d):
+    def __new__(cls, *args, **kwargs):
+        """Convenient wrapper around Analogs3d from the pyomeca library"""
+        return super(Analogs3dOsim, cls).__new__(cls, *args, **kwargs)
+
+    def __array_finalize__(self, obj):
+        super().__array_finalize__(obj)
+        # Allow slicing
+        if obj is None or not isinstance(obj, Analogs3dOsim):
+            return
+
+    def to_sto(self, file_name):
+        """
+        Write a sto file from a Analogs3dOsim
+        Parameters
+        ----------
+        file_name : string
+            path of the file to write
+        """
+        file_name = Path(file_name)
+        # Make sure the directory exists, otherwise create it
+        if not file_name.parents[0].is_dir():
+            file_name.parents[0].mkdir()
+
+        table = osim.TimeSeriesTable()
+
+        # set metadata
+        table.setColumnLabels(self.get_labels)
+
+        time_vector = np.arange(start=0, stop=1 / self.get_rate * self.shape[2], step=1 / self.get_rate)
+
+        for iframe in range(self.shape[-1]):
+            a = self.get_frame(iframe)
+            row = osim.RowVector(a.ravel().tolist())
+            table.appendRow(time_vector[iframe], row)
+        #
+        adapter = osim.STOFileAdapter()
         adapter.write(table, str(file_name))
