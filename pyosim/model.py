43fc2cc56c37a8aa8d1a25aef4c498dfbda6eb4c
romain <martinez.staps@gmail.com>
2018-05-02 14:03:40 -0400

    model inherits from osim.Model

--- pyosim/model.py
+++ pyosim/model.py
@@ -1,20 +1,8 @@
-from pathlib import Path
-
 import opensim as osim
 
 
-class Model:
+class Model(osim.Model):
     """Wrapper around opensim's osim models."""
-
-    def __init__(self, path):
-        self.path = Path(path)
-        if not self.path.is_file():
-            raise ValueError(f'{path} does not exist')
-
-        self.model = osim.Model(str(path))
-        print(f'\t{self.model.getName()} model loaded')
-
-        self.strengthen(factor=3, output=self.path.parents[0] / 'test.xml')
 
     def strengthen(self, factor, output):
         """
@@ -27,8 +15,10 @@ class Model:
         output : str
             New model path
         """
-        new_model = self.model.clone()
+        new_model = self.clone()
+        output = str(output)
         for i in range(new_model.getMuscles().getSize()):
             current_muscle = new_model.getMuscles().get(i)
             current_muscle.setMaxIsometricForce(current_muscle.getMaxIsometricForce() * factor)
-        new_model.printToXML(str(output))
+        new_model.printToXML(output)
+        print(f'{output} created')
