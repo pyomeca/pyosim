1d2dfb8458cd5fd86e05abb6fe6a01026103e15c
Romain Martinez <martinez.staps@gmail.com>
2018-05-09 21:32:02 -0400

    AnalyzeTool is now a generic class to run SO, MA & JR

--- pyosim/analyse_tool.py
+++ pyosim/analyse_tool.py
@@ -1,14 +1,16 @@
 """
-Static optimization class in pyosim
+Analyze tool class in pyosim.
+Used in static optimization, muscle analysis and joint reaction analysis.
 """
 from pathlib import Path
 
 import opensim as osim
 
 
-class SO:
+class AnalyzeTool:
     """
-    Static Optimization in pyosim
+    Analyze tool in pyosim.
+    Used in static optimization, muscle analysis and joint reaction analysis.
 
     Parameters
     ----------
@@ -18,27 +20,30 @@ class SO:
         Path to the generic so xml
     xml_output : str
         Output path of the so xml
-    xml_forces : str
-        Path to the generic forces sensor xml
-    forces_dir : str
-        Path of the directory containing the forces files (`.sto`)
+    xml_forces : str, optional
+        Path to the generic forces sensor xml (Optional)
+    ext_forces_dir : str, optional
+        Path of the directory containing the external forces files (`.sto`) (Optional)
+    muscle_forces_dir : str, optional
+        Path of the directory containing the muscle forces files (`.sto`) (Optional)
     mot_files : str, Path, list
         Path or list of path to the directory containing the motion files (`.mot`)
     sto_output : Path, str
         Output directory
-    xml_actuators
+    xml_actuators: Path, str
+        Actuators (Optional)
     prefix : str, optional
-        Optional prefix to put in front of the output filename (typically model name)
+        Optional prefix to put in front of the output filename (typically model name) (Optional)
     low_pass : int, optional
-        Cutoff frequency for an optional low pass filter on coordinates
-    remove_empty_files : bool
-        remove empty files i in `sto_output` if True
+        Cutoff frequency for an optional low pass filter on coordinates (Optional)
+    remove_empty_files : bool, optional
+        remove empty files i in `sto_output` if True (Optional)
 
     Examples
     --------
     >>> from pathlib import Path
     >>>
-    >>> from pyosim import SO
+    >>> from pyosim import AnalyzeTool
     >>>
     >>> PROJECT_PATH = Path('../Misc/project_sample')
     >>> TEMPLATES_PATH = PROJECT_PATH / '_templates'
@@ -53,11 +58,12 @@ class SO:
     >>>     'xml_output': f"{(PROJECT_PATH / participant / '_xml' / model).resolve()}_so.xml",
     >>>     'xml_forces': f"{(TEMPLATES_PATH / 'forces_sensor.xml').resolve()}",
     >>>     'xml_actuators': f"{(TEMPLATES_PATH / f'{model}_actuators.xml').resolve()}",
-    >>>     'forces_dir': f"{(PROJECT_PATH / participant / '0_forces').resolve()}",
+    >>>     'ext_forces_dir': f"{(PROJECT_PATH / participant / '0_forces').resolve()}",
+    >>>     'muscle_forces_dir': f"{(PROJECT_PATH / participant / '3_static_optimization').resolve()}",
     >>>     'sto_output': f"{(PROJECT_PATH / participant / '3_static_optimization').resolve()}",
     >>> }
     >>>
-    >>> SO(
+    >>> AnalyzeTool(
     >>>     **path_kwargs,
     >>>     mot_files=trials,
     >>>     prefix=model,
@@ -65,20 +71,32 @@ class SO:
     >>> )
     """
 
-    def __init__(self, model_input, xml_input, xml_output, xml_forces, forces_dir, mot_files, sto_output,
-                 xml_actuators=None, prefix=None, low_pass=None, remove_empty_files=False):
+    def __init__(
+            self,
+            model_input,
+            xml_input,
+            xml_output,
+            sto_output,
+            mot_files,
+            xml_forces=None,
+            ext_forces_dir=None,
+            muscle_forces_dir=None,
+            xml_actuators=None,
+            prefix=None,
+            low_pass=None,
+            remove_empty_files=False
+    ):
         self.model_input = model_input
-        self.sto_output = sto_output
         self.xml_input = xml_input
         self.xml_output = xml_output
+        self.sto_output = sto_output
         self.xml_forces = xml_forces
+        self.ext_forces_dir = ext_forces_dir
+        self.muscle_forces_dir = muscle_forces_dir
         self.xml_actuators = xml_actuators
-        self.forces_dir = forces_dir
+        self.prefix = prefix
         self.low_pass = low_pass
         self.remove_empty_files = remove_empty_files
-
-        if prefix:
-            self.prefix = prefix
 
         if not isinstance(mot_files, list):
             self.mot_files = [mot_files]
@@ -120,29 +138,46 @@ class SO:
                 analyze_tool.getAnalysisSet().get(0).setStartTime(start)
                 analyze_tool.getAnalysisSet().get(0).setEndTime(end)
 
-                # external loads file
-                loads = osim.ExternalLoads(model, self.xml_forces)
-                if self.prefix:
-                    loads.setDataFileName(
-                        f"{Path(self.forces_dir, ifile.stem.replace(f'{self.prefix}_', '')).resolve()}.sto"
-                    )
-                else:
-                    loads.setDataFileName(
-                        f"{Path(self.forces_dir, ifile.stem).resolve()}.sto"
-                    )
-                loads.setExternalLoadsModelKinematicsFileName(f'{ifile.resolve()}')
-
-                temp_xml = Path('temp.xml')
-                loads.printToXML(f'{temp_xml.resolve()}')  # temporary xml file
-
                 analyze_tool.setCoordinatesFileName(f'{ifile.resolve()}')
-                analyze_tool.setExternalLoadsFileName(f'{temp_xml}')
 
-                state = model.initSystem()
-                analyze_tool.setStatesFromMotion(state, motion, True)
+                # external loads file
+                if self.xml_forces:
+                    loads = osim.ExternalLoads(model, self.xml_forces)
+                    if self.prefix:
+                        loads.setDataFileName(
+                            f"{Path(self.ext_forces_dir, ifile.stem.replace(f'{self.prefix}_', '')).resolve()}.sto"
+                        )
+                    else:
+                        loads.setDataFileName(
+                            f"{Path(self.ext_forces_dir, ifile.stem).resolve()}.sto"
+                        )
+                    loads.setExternalLoadsModelKinematicsFileName(f'{ifile.resolve()}')
+                    temp_xml = Path('temp.xml')
+                    loads.printToXML(f'{temp_xml.resolve()}')  # temporary xml file
+                    analyze_tool.setExternalLoadsFileName(f'{temp_xml}')
+
+                if self.muscle_forces_dir:
+                    temp_2_xml = Path('temp_2.xml')
+                    analyze_tool.printToXML(f'{temp_2_xml.resolve()}')
+                    # temporary hack: edit xml to set force_file
+                    import xml.etree.ElementTree as ET
+                    tree = ET.parse(f'{temp_2_xml.resolve()}')
+                    root = tree.getroot()
+                    attr = root.find("./AnalyzeTool/AnalysisSet/objects/JointReaction/forces_file")
+                    attr.text = f"{Path(self.muscle_forces_dir, ifile.stem).resolve()}_StaticOptimization_force.sto"
+                    tree.write(f'{temp_2_xml.resolve()}')
+                    analyze_tool = osim.AnalyzeTool(f'{temp_2_xml.resolve()}', False)
+
+                    model = osim.Model(self.model_input)
+                    analyze_tool.setModel(model)
+                    analyze_tool.setModelFilename(model.getDocumentFileName())
+                    temp_2_xml.unlink()
 
                 if self.low_pass:
                     analyze_tool.setLowpassCutoffFrequency(self.low_pass)
+
+                state = model.initSystem()
+                analyze_tool.setStatesFromMotion(state, motion, True)
 
                 # set name of input (mot) file and output (sto)
                 analyze_tool.setName(f'{ifile.stem}')
@@ -161,13 +196,13 @@ class SO:
     def _remove_empty_files(directory, threshold=1000):
         """
         Remove empty files from a directory.
-        
+
         Parameters
         ----------
         directory : str, Path
             directory
         threshold : int
-            threshold in bytes 
+            threshold in bytes
         """
         for ifile in Path(directory).iterdir():
             if ifile.stat().st_size < threshold:
--- pyosim/muscle_analysis.py
+++ pyosim/muscle_analysis.py
@@ -1,10 +1,10 @@
 """
-Muscle Analysis class in pyosim
+Muscle analysis class in pyosim
 """
-from pyosim import SO
+from pyosim import AnalyzeTool
 
 
-class MA(SO):
+class MuscleAnalysis(AnalyzeTool):
     """
     Static Optimization in pyosim
 
@@ -16,27 +16,30 @@ class MA(SO):
         Path to the generic so xml
     xml_output : str
         Output path of the so xml
-    xml_forces : str
-        Path to the generic forces sensor xml
-    forces_dir : str
-        Path of the directory containing the forces files (`.sto`)
+    xml_forces : str, optional
+        Path to the generic forces sensor xml (Optional)
+    ext_forces_dir : str, optional
+        Path of the directory containing the external forces files (`.sto`) (Optional)
+    muscle_forces_dir : str, optional
+        Path of the directory containing the muscle forces files (`.sto`) (Optional)
     mot_files : str, Path, list
         Path or list of path to the directory containing the motion files (`.mot`)
     sto_output : Path, str
         Output directory
-    xml_actuators
+    xml_actuators: Path, str
+        Actuators (Optional)
     prefix : str, optional
-        Optional prefix to put in front of the output filename (typically model name)
+        Optional prefix to put in front of the output filename (typically model name) (Optional)
     low_pass : int, optional
-        Cutoff frequency for an optional low pass filter on coordinates
-    remove_empty_files : bool
-        remove empty files i in `sto_output` if True
+        Cutoff frequency for an optional low pass filter on coordinates (Optional)
+    remove_empty_files : bool, optional
+        remove empty files i in `sto_output` if True (Optional)
 
     Examples
     --------
     >>> from pathlib import Path
     >>>
-    >>> from pyosim import MA
+    >>> from pyosim import MuscleAnalysis
     >>>
     >>> PROJECT_PATH = Path('../Misc/project_sample')
     >>> TEMPLATES_PATH = PROJECT_PATH / '_templates'
@@ -51,11 +54,11 @@ class MA(SO):
     >>>     'xml_output': f"{(PROJECT_PATH / participant / '_xml' / model).resolve()}_ma.xml",
     >>>     'xml_forces': f"{(TEMPLATES_PATH / 'forces_sensor.xml').resolve()}",
     >>>     'xml_actuators': f"{(TEMPLATES_PATH / f'{model}_actuators.xml').resolve()}",
-    >>>     'forces_dir': f"{(PROJECT_PATH / participant / '0_forces').resolve()}",
+    >>>     'ext_forces_dir': f"{(PROJECT_PATH / participant / '0_forces').resolve()}",
     >>>     'sto_output': f"{(PROJECT_PATH / participant / '4_muscle_analysis').resolve()}",
     >>> }
     >>>
-    >>> MA(
+    >>> MuscleAnalysis(
     >>>     **path_kwargs,
     >>>     mot_files=trials,
     >>>     prefix=model,
