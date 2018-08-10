e69bcbc4a8c91e8ad90d8197cbda64b46142022d
Pariterre <pariterre@hotmail.com>
2018-08-10 15:49:04 -0400

    Automatically generated commit...

---
+++ examples/all_analyses.py
@@ -0,0 +1,19 @@
+import _0_project
+# import _1_markers
+# import _2_emg
+# import _3_forces
+# import _4_scaling
+# import _5_inverse_kinematics
+# import _6_inverse_dynamics
+# import _7_static_optimization
+# import _8_muscle_analysis
+# import _9_joint_reaction
+#
+
+
+def main(participant_to_do):
+    print("coucou " + participant_to_do)
+    # if not _0_project.main(specific_participant=participant_to_do, erase_previous_project=True):
+    #     print("Program failed in _0_project")
+    #     exit(1)
+
